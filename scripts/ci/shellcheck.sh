024060e7176ff687dfc3fde959f2a69d35854836
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-11-22 18:19:17 +0300

    chores: add more shell checks and shfmt
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/ci/shellcheck.sh
+++ scripts/ci/shellcheck.sh
@@ -2,3 +2,6 @@
 echo "Running shellcheck"
 find . -type f \( -name "*.sh" \) -print0 | xargs -r -0 shellcheck
 echo "OK"
+echo "Running shfmt"
+find . -type f \( -name "*.sh" \) -print0 | xargs -r -0 shfmt -d .
+echo "OK"
