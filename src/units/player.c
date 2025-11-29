635cc72f9b7652156f3e694ddf9558dd9a759c8d
homka122 <kombaev02@gmail.com>
2025-11-29 19:08:13 +0300

    Feat: add player unit initilization
    Signed-off-by: homka122 <kombaev02@gmail.com>

---
+++ src/units/player.c
@@ -0,0 +1,27 @@
+#include "units.h"
+
+Player player_create() {
+  Player result = {0};
+
+  result.type = UNIT_PLAYER;
+  result.position = (Vector){0.0, 0.0};
+  result.movement = (Vector){0.0, 0.0};
+  result.state = PLAYER_IDLE;
+
+  Sprite *frames =
+      load_spritesheet_frames("../../assets/units/player.png", 16, 15, 1, 1.0);
+  result.spritesheet = (SpriteSheet){.frames = frames, .frames_count = 1};
+  result.stat_movespeed = 1.0;
+  result.stat_max_hp = 100.0;
+  result.stat_hp = 100.0;
+  result.stat_level = 1.0;
+  result.stat_experience = 1.0;
+  result.stat_damage = 5.0;
+  result.stat_proj_count = 1.0;
+
+  result.invincibility_count = 0;
+
+  return result;
+}
+
+void player_free() { return; }
