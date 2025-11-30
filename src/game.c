8e01c8445132eb9105757762b265ba3c2335e98c
homka122 <kombaev02@gmail.com>
2025-11-30 22:18:42 +0300

    Refactor: move state to game object
    Signed-off-by: homka122 <kombaev02@gmail.com>

--- src/game.c
+++ src/game.c
@@ -1,76 +1,113 @@
-// #include "game.h"
-// #include "dyn_objs.h"
-// #include "static_objs.h"
-// #include <../vendor/GTA-VI/include/engine/coordinates.h>
-// #include <../vendor/GTA-VI/include/engine/engine.h>
-// #include <../vendor/GTA-VI/include/engine/input.h>
-// #include <../vendor/GTA-VI/include/engine/map.h>
-// #include <stb_ds.h>
-// #include <stdlib.h>
+#include "game.h"
+#include "dyn_objs.h"
+#include "state/state.h"
+#include "static_objs.h"
+#include <engine/coordinates.h>
+#include <engine/engine.h>
+#include <engine/input.h>
+#include <engine/map.h>
+#include <stb_ds.h>
+#include <stdlib.h>
 
-// #define MAP_WIDTH 50
-// #define MAP_HEIGHT 50
-// #define STATIC_OBJ_COUNT 100
+#define MAP_WIDTH 40
+#define MAP_HEIGHT 40
+#define STATIC_OBJ_COUNT 100
 
-// void game_free(Game *game);
+void game_free(Game *game);
 
-// Game *game_create() {
-//   Game *game = calloc(1, sizeof(Game));
-//   if (!game) { return NULL; }
+GameObject **get_game_objects_from_state(GlobalState *state) {
+  GameObject **objects =
+      calloc(sizeof(GameObject *),
+             state->enemies_count + state->projectiles_count + 1);
 
-//   Engine *engine = engine_create(800, 600, "Survie Among Zombies");
-//   game->engine = engine;
-//   if (!engine) {
-//     game_free(game);
-//     return NULL;
-//   }
+  GameObject *player = calloc(sizeof(GameObject), 1);
+  player->position = state->player.position;
+  player->cur_sprite = &state->player.spritesheet.frames[0];
 
-//   Map *map = map_create(MAP_WIDTH, MAP_HEIGHT);
-//   if (!map) {
-//     game_free(game);
-//     map_free(map);
-//     return NULL;
-//   };
-//   engine_set_map(engine, map);
+  objects[0] = player;
 
-//   DynamicObjects *dyn_objs = create_dynamic_objects(map);
-//   StaticObjects *st_objs = create_static_objs(map, STATIC_OBJ_COUNT);
-//   game->st_objs = st_objs;
-//   game->dyn_objs = dyn_objs;
-//   if (!dyn_objs || !st_objs) {
-//     game_free(game);
-//     map_free(map);
-//     return NULL;
-//   }
+  for (size_t i = 0; i < state->enemies_count; i++) {
+    GameObject *enemy = calloc(sizeof(GameObject), 1);
+    enemy->position = state->enemies[i].position;
+    enemy->cur_sprite = &state->enemies[i].spritesheet.frames[0];
 
-//   game->dyn_objs = dyn_objs;
-//   game->st_objs = st_objs;
-//   game->player = dyn_objs_get_player(dyn_objs);
-//   engine_set_player(engine, game->player);
+    objects[i + 1] = enemy;
+  }
 
-//   game->objects = NULL;
-//   for (int i = 0; i < arrlen(st_objs->objects); i++) {
-//     arrpush(game->objects, &st_objs->objects[i]);
-//   }
-//   GameObject *dyn_objs_arr = dyn_objs_get_objects(dyn_objs);
-//   for (int i = 0; i < arrlen(dyn_objs_arr); i++) { arrpush(game->objects,
-//   &dyn_objs_arr[i]); }
+  for (size_t i = 0; i < state->projectiles_count; i++) {
+    GameObject *projectile = calloc(sizeof(GameObject), 1);
+    projectile->position = state->projectiles[i].position;
+    projectile->cur_sprite = &state->projectiles[i].spritesheet.frames[0];
 
-//   return game;
-// }
+    objects[i + state->enemies_count + 1] = projectile;
+  }
 
-// void game_free(Game *game) {
-//   if (!game) return;
+  return objects;
+}
 
-//   if (game->dyn_objs) free_dyn_objects(game->dyn_objs);
-//   if (game->st_objs) free_static_objs(game->st_objs);
-//   if (game->objects) arrfree(game->objects);
-//   if (game->engine) engine_free(game->engine);
-//   free(game);
-// }
+Game *game_create() {
+  Game *game = calloc(1, sizeof(Game));
+  if (!game) {
+    return NULL;
+  }
 
-// void game_update(Game *game, Input *input) {
-//   if (!game || !input) return;
+  Engine *engine = engine_create(800, 600, "Survie Among Zombies");
+  game->engine = engine;
+  if (!engine) {
+    game_free(game);
+    return NULL;
+  }
 
-//   dyn_objs_update(game->dyn_objs, input, ENGINE_LOGIC_STEP);
-// }
+  TilesInfo ti = {0};
+  ti.tile_sprites = calloc(1, sizeof(Sprite));
+  ti.tile_sprites[0] = load_sprite("assets/static/grass.png", 4.0f);
+  printf("%p\n", ti.tile_sprites[0]);
+  ti.sprite_count = 1;
+  ti.tiles = calloc(MAP_WIDTH * MAP_HEIGHT, sizeof(uint32_t));
+  ti.sides_height = 32;
+
+  Map *map = map_create(MAP_WIDTH, MAP_HEIGHT, ti);
+  if (!map) {
+    game_free(game);
+    map_free(map);
+    return NULL;
+  };
+  engine_set_map(engine, map);
+
+  GlobalState globalState = init_global_state(map);
+  GameObject **objects = get_game_objects_from_state(&globalState);
+
+  engine_set_player(engine, objects[0]);
+
+  game->batch = (RenderBatch){.obj_count = 1 + game->state.enemies_count +
+                                           game->state.enemies_count,
+                              .ui_count = 0,
+                              .uis = NULL};
+  game->engine = engine;
+  game->fonts = NULL;
+  game->map = map;
+  game->state = globalState;
+
+  return game;
+}
+
+void game_free(Game *game) {
+  if (!game)
+    return;
+
+  free(game);
+}
+
+void game_update(Game *game, Input *input) {
+  if (!game || !input)
+    return;
+
+  make_step(&game->state, *input);
+  GameObject **objects = get_game_objects_from_state(&(game->state));
+  game->batch = (RenderBatch){.obj_count = 1 + game->state.enemies_count +
+                                           game->state.projectiles_count,
+                              .objs = objects,
+                              .ui_count = 0,
+                              .uis = NULL};
+  engine_set_player(game->engine, game->batch.objs[0]);
+}
