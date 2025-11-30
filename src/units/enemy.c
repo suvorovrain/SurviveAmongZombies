95dd0726ff55cb19e10207dd06f848a88ca04f74
homka122 <kombaev02@gmail.com>
2025-11-30 16:50:02 +0300

    Feat: add enemy.c
    Signed-off-by: homka122 <kombaev02@gmail.com>

---
+++ src/units/enemy.c
@@ -0,0 +1,22 @@
+#include "units.h"
+
+Enemy enemy_create(Vector position) {
+  Enemy result = {0};
+
+  result.type = UNIT_ENEMY;
+  result.position = position;
+  result.movement = (Vector){0.0, 0.0};
+  result.state = ENEMY_IDLE;
+
+  Sprite *frames =
+      load_spritesheet_frames("../../assets/units/imp_d.png", 16, 15, 1, SCALE);
+  result.spritesheet = (SpriteSheet){.frames = frames, .frames_count = 1};
+  result.stat_movespeed = 1.5;
+  result.stat_max_hp = 100.0;
+  result.stat_hp = 100.0;
+  result.stat_damage = 5.0;
+
+  return result;
+}
+
+void enemy_free() { return; }
