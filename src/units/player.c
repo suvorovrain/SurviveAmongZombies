2f0696eb635e547f3b93297b6f491f6419d62d2b
homka122 <kombaev02@gmail.com>
2025-12-01 17:22:36 +0300

    Feat: add animations
    Signed-off-by: homka122 <kombaev02@gmail.com>

--- src/units/player.c
+++ src/units/player.c
@@ -9,19 +9,24 @@ Player player_create(Vector position) {
   result.state = PLAYER_IDLE;
 
   Sprite *frames =
-      load_spritesheet_frames("assets/units/player.png", 16, 15, 1, SCALE);
-  result.spritesheet = (SpriteSheet){.frames = frames, .frames_count = 1};
+      load_spritesheet_frames("assets/units/player.png", 16, 15, 16, SCALE);
+  result.spritesheet = (SpriteSheet){.frames = frames, .frames_count = 16};
+  Sprite *frames_move =
+      load_spritesheet_frames("assets/units/player.png", 16, 15, 16, SCALE);
+  result.spritesheet_move = (SpriteSheet){.frames = frames, .frames_count = 16};
   result.stat_movespeed = 3.0;
   result.stat_max_hp = 100.0;
   result.stat_hp = 100.0;
   result.stat_level = 1.0;
   result.stat_attack_speed = 0.75;
   result.stat_experience = 1.0;
-  result.stat_piercing = 3;
+  result.stat_piercing = 1;
   result.stat_damage = 30.0;
   result.stat_proj_count = 2.0;
 
   result.invincibility_count = 0;
+  result.direction_face = DIRECTION_DOWN;
+  result.direction_side = DIRECTION_RIGHT;
 
   return result;
 }
