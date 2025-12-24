081455b00ba5f9ed1f85c201937a477fec9a9ff7
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-12-24 18:30:14 +0300

    fix: nits
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/ci/code-format-check.sh
+++ scripts/ci/code-format-check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 echo "Check the code formatting"
-find src tests -type f \( -name "*.c" -o -name "*.h" \) -print0 | xargs -r -0 clang-format-20 --dry-run --Werror
+find src -type f \( -name "*.c" -o -name "*.h" \) -print0 | xargs -r -0 clang-format-20 --dry-run --Werror
 echo "OK"
